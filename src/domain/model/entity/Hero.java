dc68972093f9c96fdefe9bf77384f8a5c2f5c320
boratoker <toker2003@gmail.com>
2026-04-08 19:33:42 +0300

    hero class added

---
+++ src/domain/model/entity/Hero.java
@@ -0,0 +1,44 @@
+package domain.model.entity;
+
+import java.util.Random;
+
+public class Hero {
+    private int hp = 17;
+    private int mana = 80;
+    private int def = 2;
+    private int str;
+    private int energy = 100;
+
+    public Hero() {
+        this.str = new Random().nextInt(8) + 8;
+    }
+
+    public boolean attemptBreak() {
+        int energyCost = 10;
+        if (this.energy >= energyCost) {
+            this.energy -= energyCost;
+            return new Random().nextInt(20) < this.str;
+        }
+        return false;
+    }
+
+    public int calculateDamage(int weaponAtk) {
+        return (this.str * 2) + (weaponAtk * 3);
+    }
+
+    public void consumeEnergyForMove() {
+        this.energy -= 1;
+    }
+
+    public void heal(int amount) {
+        this.hp += amount;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getStr() {
+        return str;
+    }
+}

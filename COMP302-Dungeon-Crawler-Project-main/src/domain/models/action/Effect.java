a9a4fb37e5a9b7246b4ac64af8b71aec5a58ba5d
Mertalpi <mkalender23@ku.edu.tr>
2026-04-21 21:11:08 +0300

    Fix nested directory structure

--- COMP302-Dungeon-Crawler-Project-main/src/domain/models/action/Effect.java
+++
@@ -1,9 +0,0 @@
-package domain.models.action;
-
-import domain.models.entity.GameObject;
-import domain.models.entity.Hero;
-import domain.models.map.GameMap;
-
-public interface Effect {
-    void apply(GameObject target, Hero hero, GameMap map);
-}
--- COMP302-Dungeon-Crawler-Project-main/src/domain/models/action/Interactable.java
+++
@@ -1,7 +0,0 @@
-package domain.models.action;
-
-import java.util.List;
-
-public interface Interactable {
-    List<Action> getActions();
-}
